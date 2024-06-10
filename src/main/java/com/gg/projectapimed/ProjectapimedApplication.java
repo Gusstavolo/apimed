ed07b6f1bcefd35c066337a0ee49cd19ef6b5c6c
Gustavo Oli <69ggustavoo69@gmail.com>
2024-06-10 10:38:19 -0300

    first commit

---
+++ src/main/java/com/gg/projectapimed/ProjectapimedApplication.java
@@ -0,0 +1,13 @@
+package com.gg.projectapimed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectapimedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectapimedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gg/projectapimed/ProjectapimedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gg.projectapimed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectapimedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
