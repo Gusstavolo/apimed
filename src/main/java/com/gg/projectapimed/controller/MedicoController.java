c784fae7ecfeff2fbec6c724b1f25556d87196ce
Gustavo Oli <69ggustavoo69@gmail.com>
2024-06-12 09:23:33 -0300

    update repository

--- src/main/java/com/gg/projectapimed/controller/MedicoController.java
+++ src/main/java/com/gg/projectapimed/controller/MedicoController.java
@@ -2,6 +2,10 @@ package com.gg.projectapimed.controller;
 
 
 import com.gg.projectapimed.medico.DadosCadastroMedico;
+import com.gg.projectapimed.medico.Medico;
+import com.gg.projectapimed.medico.MedicoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
+
     @PostMapping
+    @Transactional
     public void Cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save( new Medico(dados) );
     }
 
 
---
+++ src/main/java/com/gg/projectapimed/medico/MedicoRepository.java
@@ -0,0 +1,6 @@
+package com.gg.projectapimed.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+}
